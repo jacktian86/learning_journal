d4a3c4a271f52779af050c5705370a62d1e67402
xbobo <jack.tian@gmail.com>
2014-06-05 02:51:55 +0000

    update

--- journal.py
+++ journal.py
@@ -106,5 +106,5 @@ def teardown_request(exception):
 
 # put this at the very bottom of the file.
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host="0.0.0.0",debug=True)
+    app.run(debug=True)
+    #app.run(host="0.0.0.0",debug=True)
